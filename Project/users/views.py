26f548a34b44d13796ce4fad54d6037e09c2c597
Mojanne <mozhan_omidi@yahoo.com>
2020-07-28 02:20:02 +0430

    form styling

--- Project/users/views.py
+++ Project/users/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 
 #We use Django's default form
-def reguster(request):
-    form = UserCreationForm
+#if its a POST request validate it if not create blank form
+#form.cleaned_data returns a dictionary of validated form input fields and their values, where string primary keys are returned as objects
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created For You, Dear {username}!')
+            return redirect('blog-homepage')
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/registration.html', {'form': form})
 
 
